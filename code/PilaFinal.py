cf2112f74d7eafab54b33319b1a2841eeb6fa40f
luisalejandromedinab <luisalejandromedinab@gmail.com>
2024-06-23 15:34:25 +0000

    creación de pila final casi listo

--- code/PilaFinal.py
+++ code/PilaFinal.py
@@ -4,6 +4,7 @@ import re as regex
 pila = ["Fin"]
 expresion: str = ""
 contador = 0
+ingnore_symbols = ["!", ">", "<", "=", "|", "&"]
 def main():
     pila.append(procS)
     #ask for the initial exprecion 
@@ -45,22 +46,374 @@ def procS():
     pila.pop()
     añadirElemento(respuesta, 1)
     añadirElemento(getIndexOfBlanckeElement(), 0)
-    añadirElemento(procE, 0)
+    añadirElemento(procELO, 0)
     mostrar_lista_con_indices(pila, "1")
     pass
-
-def respuesta():
+#Metodos pila logica
+def procELO():
     global pila
     pila.pop()
-    #we try to convert the result to an int 
-    try:
-        resultadoFinal = pila[-1]
-        pila.pop()
-        mostrar_lista_con_indices(pila, "10")
-        print(f"Resultado: {resultadoFinal}")
-    except:
-        raise ValueError("Error Found in respuesta")
+    añadirElemento(procELO_L, 1)
+    añadirElemento(getIndexOfBlanckeElement(), 0)
+    añadirElemento(procEL2, 0)
+    mostrar_lista_con_indices(pila, "18")
     pass
+
+def procELO_L():
+    global pila 
+    global expresion
+    global ingnore_symbols
+
+    if len(expresion) == 0:
+        symbol= " "
+    else:
+        symbol = expresion[0]
+    
+    match symbol:
+        case " ":
+            pila_logico_non_operation()
+            mostrar_lista_con_indices(pila, "20")
+        case "|":
+            advance("|")
+            pila.pop()
+            temporal = pila[-1]
+            pila.pop()
+            añadirElemento(procELO_L, 1)
+            añadirElemento(getIndexOfBlanckeElement(),0)
+            añadirElemento(temporal, 1)
+            añadirElemento(operationOR,0)
+            añadirElemento(getIndexOfBlanckeElement(), 0)
+            añadirElemento(procEL2, 0)
+            mostrar_lista_con_indices(pila, "20")
+            
+
+    pass
+
+def operationOR():
+    global pila
+    pila.pop()
+    operador1 = pila[-1]
+    pila.pop()
+    operador2 = pila[-1]
+
+    if type(operador1)  == int or type(operador2) == int:
+        raise Exception("the operation Or can only be done with two booleans")
+
+    pila.pop()
+    index = pila[-1]
+    pila.pop()
+    pila[index] = operador1 or operador2
+    mostrar_lista_con_indices(pila, "OR")
+    pass
+
+def procEL2():
+    global pila 
+    pila.pop()
+    añadirElemento(procEL2_L, 1)
+    añadirElemento(getIndexOfBlanckeElement(), 0)
+    añadirElemento(procEL3, 1)
+    mostrar_lista_con_indices(pila, "19")
+    pass
+
+def procEL2_L():
+    global pila 
+    global expresion
+    global ingnore_symbols
+
+    if len(expresion) == 0:
+        symbol= " "
+    else:
+        symbol = expresion[0]
+    
+    match symbol:
+        case " " | "|":
+            pila_logico_non_operation()
+            mostrar_lista_con_indices(pila, "21")
+        case "&":
+            advance("&")
+            pila.pop()
+            temporal = pila[-1]
+            pila.pop()
+            añadirElemento(procEL2_L, 1)
+            añadirElemento(getIndexOfBlanckeElement(),0)
+            añadirElemento(temporal, 1)
+            añadirElemento(operatationAND,0)
+            añadirElemento(getIndexOfBlanckeElement(), 0)
+            añadirElemento(procEL3, 1)
+            mostrar_lista_con_indices(pila, "21")
+    pass
+
+
+def operatationAND():
+    global pila
+    pila.pop()
+    operador1 = pila[-1]
+    pila.pop()
+    operador2 = pila[-1]
+
+    if type(operador1)  == int or type(operador2) == int:
+        raise Exception("the operation AND can only be done with two booleans")
+
+    pila.pop()
+    index = pila[-1]
+    pila.pop()
+    pila[index] = operador1 and operador2
+    mostrar_lista_con_indices(pila, "AND")
+    pass
+
+def procEL3():
+    global pila
+    global expresion
+
+    symbol = expresion[0]
+
+    match symbol:
+        case "!":
+            advance("!")
+            pila.pop()
+            añadirElemento(cancel,0)
+            procER()
+        case _ if regex.match(r'[0-9]+', symbol) or symbol == "(":
+            pila.pop()
+            añadirElemento(notCancel,0)
+            procER()
+            
+    pass
+
+
+def cancel():
+    global pila
+    pila.pop()
+    resultado = pila[-1]
+    #we check if the tipe is int
+    if type(resultado) == int:
+        raise Exception("You cannot negate a number please complete the operation")
+    pila.pop()
+    index = pila[-1]
+    pila.pop()
+    pila[index] = not resultado
+    mostrar_lista_con_indices(pila, "Cancel")
+    pass
+
+def notCancel():
+    global pila
+    pila_logico_non_operation()
+    mostrar_lista_con_indices(pila, "No cancelar")
+    pass
+
+
+def pila_logico_non_operation():
+    pila.pop()
+    resultado = pila[-1]
+    pila.pop()
+    index = pila[-1]
+    pila.pop()
+    pila[index] = resultado
+    pass
+
+
+#Metodos de pila relacional 
+
+def procER():
+    global pila
+    pila_add_realcional()
+    pass
+
+def pila_add_realcional():
+    global pila
+    añadirElemento(getIndexOfBlanckeElement(), 0)
+    añadirElemento(relacional, 1)
+    añadirElemento(getIndexOfBlanckeElement(), 0)
+    añadirElemento(procE, 0)
+    mostrar_lista_con_indices(pila, "1")
+
+def relacional():
+    global pila
+    global expresion
+    if len(expresion) == 0:
+        symbol= " "
+    else:
+        symbol = expresion[0]
+    
+
+    match symbol:
+        case " " | ")":
+            relacional_non_operation()
+            pass
+        case ">":
+            advance(">")
+            procMA()
+            pass    
+        case "<":
+            advance("<")
+            procME()
+            pass
+        case "=":
+            advance("=")
+            procIG()
+            pass
+        case "!":
+            advance("!")
+            procDI()
+            pass
+        case _:
+            raise ValueError(f"Error Found in relacional with the character {expresion[0]}")
+    pass
+
+def relacional_non_operation():
+    pila.pop()
+    resultado = pila[-1]
+    pila.pop()
+    index = pila[-1]
+    pila.pop()
+    pila[index] = resultado
+    mostrar_lista_con_indices(pila, "17")
+    pass
+
+def procME():
+    global pila
+    global expresion
+    if len(expresion) == 0:
+        raise Exception("Error found in procMA we expect a number or a = and a number")
+    else:
+        symbol = expresion[0]
+
+    match symbol:
+        case "=":
+            advance("=")
+            operation_Relational(menor_igual_que)
+        case _ if regex.match(r'[0-9]+', symbol):
+            operation_Relational(menor_que)
+        case _:
+            raise Exception(f"Error found in procME with the character {expresion[0]}")
+    mostrar_lista_con_indices(pila, "Identificación Menor")
+    pass
+
+
+def procIG():
+    global pila
+    global expresion
+    print(f"que paso {expresion}")
+    if len(expresion) == 0:
+        raise Exception("Error found in procIG we expect a number or a = and a number")
+    else:
+        symbol = expresion[0]
+
+    match symbol:
+        case "=":
+            advance("=")
+            operation_Relational(igual_que)
+        case _ if regex.match(r'[0-9]+', symbol):
+            raise Exception("Error found in procIG we expect a = and a number")
+        case _:
+            raise Exception(f"Error found in procIG with the character {expresion[0]}")
+    mostrar_lista_con_indices(pila, "Identificación Igual")
+
+
+def procDI():
+    global pila
+    global expresion
+    if len(expresion) == 0:
+        raise Exception("Error found in procDI we expect a number or a = and a number")
+    else:
+        symbol = expresion[0]
+
+    match symbol:
+        case "=":
+            advance("=")
+            operation_Relational(diferente_que)
+        case _ if regex.match(r'[0-9]+', symbol):
+            raise Exception("Error found in procDI we expect a = and a number")
+        case _:
+            raise Exception(f"Error found in procDI with the character {expresion[0]}")
+    mostrar_lista_con_indices(pila, "Identificación Diferente")
+
+    
+
+    
+
+def procMA():
+    global pila
+    global expresion
+    if len(expresion) == 0:
+       raise Exception("Error found in procMA we expect a number or a = and a number")
+    else:
+        symbol = expresion[0]
+
+    match symbol:
+        case "=":
+            advance("=")
+            operation_Relational(mayor_igual_que)
+        case _ if regex.match(r'[0-9]+', symbol):
+            operation_Relational(mayor_que)
+        case _:
+            raise Exception(f"Error found in procMA with the character {expresion[0]}")
+    mostrar_lista_con_indices(pila, "Identificación Mayor")
+    pass
+
+def operation_Relational(operation):
+    global pila
+    pila.pop()
+    temporal = pila[-1]
+    pila.pop()
+    añadirElemento(temporal, 1)
+    añadirElemento(operation, 0)
+    añadirElemento(getIndexOfBlanckeElement(), 0)
+    añadirElemento(procE, 0)
+    mostrar_lista_con_indices(pila, "18")
+    pass
+    
+
+def mayor_que():
+    global pila
+    operador1, operador2, index = pila_movement_relacional()
+    pila[index] = operador1 > operador2
+    mostrar_lista_con_indices(pila, "Mayor_que")
+    pass
+
+def mayor_igual_que():
+    global pila
+    operador1, operador2, index = pila_movement_relacional()
+    pila[index] = operador1 >= operador2
+    mostrar_lista_con_indices(pila, "Mayor_igual_que")
+    pass
+
+def menor_que():
+    global pila
+    operador1, operador2, index = pila_movement_relacional()
+    pila[index] = operador1 < operador2
+    pass
+
+def menor_igual_que():
+    global pila
+    operador1, operador2, index = pila_movement_relacional()
+    pila[index] = operador1 <= operador2
+    pass
+
+def igual_que():
+    global pila
+    operador1, operador2, index = pila_movement_relacional()
+    pila[index] = operador1 == operador2
+    pass
+
+def diferente_que():
+    global pila
+    operador1, operador2, index = pila_movement_relacional()
+    pila[index] = operador1 != operador2
+    pass
+
+def pila_movement_relacional() -> list:
+    global pila
+    pila.pop()
+    operador1 = pila[-1]
+    pila.pop()
+    operador2 = pila[-1]
+    pila.pop()
+    index = pila[-1]
+    pila.pop()
+    return [operador1, operador2, index]
+
+#metodos de pila aritmetica 
 
 def procE():
     global pila 
@@ -75,6 +428,7 @@ def procEL():
     global pila 
     global expresion
     global contador
+    global ingnore_symbols
     if len(expresion) == 0:
         symbol= " "
     else:
@@ -96,6 +450,8 @@ def procEL():
             mostrar_lista_con_indices(pila, "9")
             pass
         case " ":
+            pila_non_operation_EL()
+        case _ if symbol in ingnore_symbols:
             pila_non_operation_EL()
         case _:
             raise ValueError(f"Error Found in procEL with the character {expresion[0]}")
@@ -159,6 +515,7 @@ def procT():
 def procTL():
     global pila
     global expresion
+    global ingnore_symbols
     if len(expresion) == 0:
         symbol= " "
     else:
@@ -175,6 +532,8 @@ def procTL():
         case ")":
             pila_non_operation_TL()
         case "+" | "-" | "−":
+            pila_non_operation_TL()
+        case _ if symbol in ingnore_symbols:
             pila_non_operation_TL()
         case _:
             raise ValueError(f"Error Found in procTL with the character {expresion[0]}")
@@ -240,6 +599,7 @@ def procP():
 def procPL():
     global pila 
     global expresion
+    global ingnore_symbols
     if len(expresion) == 0:
         symbol= " "
     else:
@@ -254,6 +614,8 @@ def procPL():
         case ")":
             pila_non_operation_PL()
         case "+" | "-" | "*" | "/" | "−":
+            pila_non_operation_PL()
+        case _ if symbol in ingnore_symbols:
             pila_non_operation_PL()
         case _:
             raise ValueError(f"Error Found in procPL with the character {expresion[0]}")
@@ -328,6 +690,8 @@ def procF():
             pila.pop()
             añadirElemento(")", 0)
             añadirElemento(temporal, 0)
+            añadirElemento(relacional, 1)
+            añadirElemento(getIndexOfBlanckeElement(), 0)
             añadirElemento(procE, 0)
             advance("(")
             mostrar_lista_con_indices(pila, "5")
@@ -362,7 +726,7 @@ def get_operator() -> int:
     global expresion
     operator:str = ""
     for i in expresion:
-        if i in ["+", "*", ")", "^", "−", "-", "/"]: 
+        if i in ["+", "*", ")", "^", "−", "-", "/", "<", ">", "=", "!", "|", "&"]: 
             break
         else:
             operator += i
@@ -397,6 +761,19 @@ def check(cadena: str) -> bool:
             raise ValueError(f"Invalid expresion it contains {result}")
 
     return True
+
+def respuesta():
+    global pila
+    pila.pop()
+    #we try to convert the result to an int 
+    try:
+        resultadoFinal = pila[-1]
+        pila.pop()
+        mostrar_lista_con_indices(pila, "10")
+        print(f"Resultado: {resultadoFinal}")
+    except:
+        raise ValueError("Error Found in respuesta")
+    pass
 
 
 main()
--- code/test.py
+++ code/test.py
@@ -1,4 +1,5 @@
 #print(type(eval("False & True")))
 #print(float(7-(4*(-3.5)+8.2/2.1)^2+5.1))
-print(float(7-(4*(-3.5)+8.2/2.1)**2+5.1))
-print(float((7-(101.91383219954648+5.1))))
+#print(float(-3 + (5.2 - 4.3) * (2 + 3.1 / (-0.5)) + 7.4 / (1.2 + 3.6) - 6.8) >= float(9.3 + (2.1**2 - 4 * 3.7) / (6.5 - 2.2)))
+print(False or (False and True))
+ 
